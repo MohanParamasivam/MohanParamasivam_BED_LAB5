0d2b166878dd966b34951972a3c054686355b555
MohanParamasivam <111678461+MohanParamasivam@users.noreply.github.com>
2023-01-17 15:45:13 +0530

    Delete employee-management-system directory

--- employee-management-system/src/main/java/com/gl/ems/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gl.ems.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gl.ems.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
